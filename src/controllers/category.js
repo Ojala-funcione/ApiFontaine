d6a73cdb113717faaedd2002ad64d1f0823e6be2
Jime227 <jamedina@live.com.ar>
2022-02-22 15:05:54 -0300

    fixing

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -3,13 +3,29 @@ const { db } = require("../db/db");
 const addCategory = async (req, res, next) => {
   try {
     const category = req.body;
-    console.log(category);
     const newCategory = await db.collection("category").add(category);
     res.json("Categoria añadida correctamente");
   } catch (error) {
     next(error);
   }
 };
+
+const addSubcategory = async (req, res, next) => {
+  try {
+    const info = req.body;
+    console.log(info);
+    const catRef = await db
+      .collection("category")
+      .doc(info.category)
+      .collection(info.subcategory.name)
+      .add(info.subcategory);
+    console.log(catRef);
+    res.json("Subcategoria añadida correctamente");
+  } catch (error) {
+    next(error);
+  }
+};
+
 const allCategories = async (req, res, next) => {
   try {
     const categoriesRef = await db.collection("category").get();
@@ -22,4 +38,4 @@ const allCategories = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { addCategory, allCategories };
+module.exports = { addCategory, allCategories, addSubcategory };
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-const { addCategory, allCategories } = require("../controllers/category");
+const {
+  addCategory,
+  allCategories,
+  addSubcategory,
+} = require("../controllers/category");
 
 const router = Router();
 
 router.post("/addcategory", addCategory);
 router.get("/categories", allCategories);
+router.post("/addsubcategory", addSubcategory);
 
 module.exports = router;
